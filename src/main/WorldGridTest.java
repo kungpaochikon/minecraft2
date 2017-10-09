1ae10b833e8873fac5b4e18365ec7addf2f9b73e
gary <flemingg@mail.gvsu.edu>
2017-10-09 11:45:47 -0400

    Pushing Just Because

---
+++ src/main/WorldGridTest.java
@@ -0,0 +1,20 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WorldGridTest {
+
+	@Test
+	public void test() {
+		WorldGrid world = new WorldGrid(64,64,64);
+		world.setWID(-1, -1, 1);
+		world.setWID(1,1, -1);
+		world.getWID(-1, -1);
+		world.setWID(100, 100, 1);
+		world.getWID(100, 100);
+		world.generate();
+	}
+
+}
