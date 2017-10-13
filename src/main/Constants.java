644707ad42f4bec3f7c1df85af8ba0bab4fa1f79
deleonl <deleonl@mail.gvsu.edu>
2017-10-13 04:31:41 -0400

    Added constants, integrity and swinging.
    Created a constants class for readability, made blocks take numerous
    hits, and added a pretty rough swinging animation. Also, fixed about
    half of the CheckStyle complaints (~3400 to ~1800).

---
+++ src/main/Constants.java
@@ -0,0 +1,56 @@
+package main;
+
+public final class Constants {
+	public static final int BLOCK_AIR = 0;
+	public static final int BLOCK_DIRT = 1;
+	public static final int BLOCK_GRASS = 2;
+	public static final int BLOCK_LEAVES = 3;
+	public static final int BLOCK_WATER = 4;
+	public static final int BLOCK_DIAMOND_ORE = 5;
+	public static final int BLOCK_STONE = 6;
+	public static final int BLOCK_COBBLESTONE = 7;
+	
+	public static final int TYPE_BLOCK = 0;
+	public static final int TYPE_BACK = 1;
+	public static final int TYPE_TOOL = 2;
+	public static final int TYPE_ITEM = 3;
+	public static final int TYPE_FOOD = 4;
+	public static final int TYPE_ENTITY = 5;
+	
+	public static final int ENTITY_PLAYER = 0;
+	public static final int ENTITY_CHICKEN = 1;
+	public static final int ENTITY_COW = 2;
+	public static final int ENTITY_ZOMBIE = 3;
+	
+	public static final int BACK_AIR = 0;
+	public static final int BACK_WOOD = 1;
+	public static final int BACK_DIRT = 2;
+	public static final int BACK_STONE = 3;
+	
+	public static final int PICKAXE = 0;
+	public static final int AXE = 1;
+	public static final int SWORD = 2;
+	
+	public static final int ITEM_DIAMOND = 0;
+	
+	public static final int APPLE = 0;
+	public static final int RAW_CHICKEN = 1;
+	public static final int RAW_BEEF = 2;
+	
+	public static final int[] INTEGRITIES = {Integer.MAX_VALUE, 50, 50, 20, Integer.MAX_VALUE, 100, 80, 80};
+	
+	public static final double GAME_HERTZ = 60.0;
+	
+	//Calculate how many ns each frame should take for our target game hertz.
+	public static final double TIME_BETWEEN_UPDATES = 1000000000 / Constants.GAME_HERTZ;
+	
+	//At the very most we will update the game this many times before a new render.
+    //If you're worried about visual hitches more than perfect timing, set this to 1.
+    public static final int MAX_UPDATES_BEFORE_RENDER = 100;
+	
+  //If we are able to get as high as this FPS, don't render again.
+    public static final double TARGET_FPS = 60;
+    public static final double TARGET_TIME_BETWEEN_RENDERS = 1000000000 / TARGET_FPS;
+    
+    public static final long SWING_COOL_DOWN = 200;
+}
--- src/main/WorldGrid.java
+++ src/main/WorldGrid.java
@@ -9,14 +9,14 @@ public class WorldGrid {
 	private Random random;
 	private Block[][] wGrid;
 	
-	public WorldGrid(int x,int y, int bSize){
+	public WorldGrid(int x,int y, int bSize) {
 		//Create array of empty blocks
 		sizeX = x;
 		sizeY = y;
 		blockSize = bSize;
 		wGrid = new Block[sizeX][sizeY];
-		for(int i = 0;i<sizeX;i++){
-			for(int j = 0;j<sizeY;j++){
+		for (int i = 0; i < sizeX; i++) {
+			for (int j = 0; j < sizeY; j++) {
 				wGrid[i][j] = new Block();
 			}
 		}
@@ -30,7 +30,7 @@ public class WorldGrid {
 	 * Generates the world on the grid
 	 * 
 	 ******************************************************************/
-	public void generate(){
+	public void generate() {
 	      //World Grid
 	      //Basic Ground and Trees
 		int dirtBackDepth = 20;
@@ -39,35 +39,40 @@ public class WorldGrid {
 	      int waterStreak = 0;
 	      int ground = 20;
 	      int r = 0;
-	      for(int i = 0;i<sizeX;i++){
-	    	  if(!water){
+	      for (int i = 0; i < sizeX; i++) {
+	    	  if (!water) {
 		    	  r = random.nextInt(8);
-		    	  if(r==1) ground++;
-		    	  if(r==2) ground--;
+		    	  if (r == 1) {
+		    		  ground++;
+		    	  }
+		    	  if (r == 2) {
+		    		  ground--;
+		    	  }
 	    	  }
 	    	  //Top Layer
-	    	  if(!water){
+	    	  if (!water) {
 		    	  r = random.nextInt(40);
-		    	  if(r==0){
+		    	  if (r == 0) {
 		    		  water = true;
 		    		  waterStreak = 0;
 		    	  }
-		    	  setWID(i,ground,2);
-	    	  }
-	    	  else{
+		    	  setWID(i, ground, Constants.BLOCK_GRASS);
+	    	  } else {
 	    			double xx = i;
 	    			double yy = ground;
 	    			double rr = 4;
-		    		   for(int ii = (int) (xx-rr);ii<xx+rr;ii++){
-		    		       for(int jj = (int) (yy-rr);jj<yy+rr;jj++){
-		    		           if((ii-xx)*(ii-xx)+(jj-yy)*(jj-yy)<=rr*rr){
-		    		               if(wGridBounds((int)ii,(int)jj) && jj>=ground) setWID(ii,jj,4);
+		    		   for (int ii = (int) (xx - rr); ii < xx + rr; ii++) {
+		    		       for (int jj = (int) (yy - rr); jj < yy + rr; jj++) {
+		    		           if ((ii - xx) * (ii - xx) + (jj - yy) * (jj - yy) <= rr * rr) {
+		    		               if (wGridBounds((int) ii, (int) jj) && jj >= ground) {
+		    		            	   setWID(ii, jj, Constants.BLOCK_WATER);
+		    		               }
 		    		           }
 		    		       }
 		    		   }
-	    		  if(waterStreak>3){
+	    		  if (waterStreak > 3) {
 	    			  r = random.nextInt(3);
-	    			  if(r==0){
+	    			  if (r == 0) {
 	    				  water = false;
 	    			  }
 	    		  }
@@ -75,44 +80,50 @@ public class WorldGrid {
 	    	  }
 	    	  //Gen Tree
 	    	  r = random.nextInt(12);
-	    	  if(r==0 && wGrid[i][ground].getWID()!=4 && wGrid[i][ground].getWID()!=0){
-	    		  wGenTree(i,ground,random.nextInt(3)+1,random.nextInt(5)+7,4);
+	    	  if (r == 0 && wGrid[i][ground].getWID() != Constants.BLOCK_WATER && wGrid[i][ground].getWID()
+	    			  != Constants.BLOCK_AIR) {
+	    		  wGenTree(i, ground, random.nextInt(3) + 1, random.nextInt(5) + 7, 4);
 	    	  }
-	    	  for(int j = ground+1;j<sizeY;j++){
-	    		  if(wGridBounds(i,j) && wGrid[i][j].getWID()==0){
-	    			  if(j<stoneDepth) wGrid[i][j].setWID(1);
-	    			  else wGrid[i][j].setWID(6);
+	    	  for (int j = ground + 1; j < sizeY; j++) {
+	    		  if (wGridBounds(i, j) && wGrid[i][j].getWID() == Constants.BLOCK_AIR) {
+	    			  if (j < stoneDepth) {
+	    				  wGrid[i][j].setWID(Constants.BLOCK_DIRT);
+	    			  } else {
+	    				  wGrid[i][j].setWID(Constants.BLOCK_STONE);
+	    			  }
 	    		  }
 	    		  //wGrid[i][j] = 1;
 	    		  
 	    		  //Background
-	    		  if(j>dirtBackDepth){
-	    			  if(j<stoneDepth) wGrid[i][j].setBID(2);
-	    			  else wGrid[i][j].setBID(3);
+	    		  if (j > dirtBackDepth) {
+	    			  if (j < stoneDepth) {
+	    				  wGrid[i][j].setBID(Constants.BACK_DIRT);
+	    			  } else wGrid[i][j].setBID(Constants.BACK_STONE);
 	    		  }
 	    	  }
 	      }
 	      //Caves
-	      for(int i = 0;i<sizeX;i++){
-	    	  for(int j = 30;j<sizeY;j++){
-	    		  if(random.nextInt(80)==1){
-	    			  wDeleteCircle(i, j, 3,1,1,1,0);
+	      for (int i = 0; i < sizeX; i++) {
+	    	  for (int j = 30; j < sizeY; j++) {
+	    		  if (random.nextInt(80) == 1) {
+	    			  wDeleteCircle(i, j, 3, 1, 1, 1, 0);
 	    		  }
-	    		  if(random.nextInt(1000)==1){
-	    			  wPlaceCircle(i, j, 1, 5,1,1,1,1);
+	    		  if (random.nextInt(1000) == 1) {
+	    			  wPlaceCircle(i, j, 1, Constants.BLOCK_DIAMOND_ORE, 1, 1, 1, 1);
 	    		  }
 	    	  }
 	      }
 	      //Water
-	      for(int i = 0;i<sizeX;i++){
-	    	  for(int j = 30;j<sizeY;j++){
-	    		  if(random.nextInt(80)==1 && wGridBounds(i,j) && wGridBounds(i,j+1) && wGrid[i][j+1].getWID()!=0 && wGrid[i][j].getWID()==0){
-	    			  setWID(i,j,4);
+	      for (int i = 0; i < sizeX; i++) {
+	    	  for (int j = 30; j < sizeY; j++) {
+	    		  if (random.nextInt(80) == 1 && wGridBounds(i, j) && wGridBounds(i, j + 1) && wGrid[i][j + 1].getWID()
+	    				  != 0 && wGrid[i][j].getWID() == 0) {
+	    			  setWID(i, j, 4);
 	    		  }
 	    	  }
 	      }
 	      //Update
-	      for(int i = 0;i<16;i++){
+	      for (int i = 0; i < 16; i++) {
 		      //wUpdateLighting(0, sizeX, 0, sizeY);
 		      //wUpdateWater(0,sizeX,0,sizeY);
 	      }
@@ -125,15 +136,21 @@ public class WorldGrid {
     * Spawns Tree
     * 
     *******************************************************************/
-   private void wGenTree(int x, int y, int w, int h, int r){
+   private void wGenTree(final int x, final int y, final int w, final int h, final int r) {
 	   //Trunk
-	   for(int i = x;i<x+w;i++){
-		   for(int j = y-h+1;j<=y;j++){
-			   if(wGridBounds(i,j)) wGrid[i][j].setBID(1);
+	   for (int i = x; i < x + w; i++) {
+		   for (int j = y - h + 1; j <= y; j++) {
+			   if (wGridBounds(i, j)) {
+				   wGrid[i][j].setBID(1);
+			   }
 		   }
 	   }
 	   //Leaves
-	   wPlaceCircle(x+(w/2),y-h,r,3,1,1,1,0);
+	   wPlaceCircle(x + (w / 2), y - h, r, 3, 1, 1, 1, 0);
+   }
+   
+   public Block getBlock(final int x, final int y) {
+	   return wGrid[x][y];
    }
    
    /*******************************************************************
@@ -143,14 +160,17 @@ public class WorldGrid {
     * Deletes things in a circle of radius r
     * 
     *******************************************************************/
-   private void wDeleteCircle(int x, int y, int r, int x1Off, int x2Off, int y1Off, int y2Off){
+   private void wDeleteCircle(final int x, final int y, final int r, final int x1Off,
+		   final int x2Off, final int y1Off, final int y2Off) {
 	double xx = x;
 	double yy = y;
 	double rr = r;
-   for(int i = (int) (xx-rr-x1Off);i<xx+rr+x2Off;i++){
-       for(int j = (int) (yy-rr-y1Off);j<yy+rr+y2Off;j++){
-           if((i-xx)*(i-xx)+(j-yy)*(j-yy)<=rr*rr){
-               if(wGridBounds((int)i,(int)j)) wGrid[(int)i][(int)j].setWID(0);
+   for (int i = (int) (xx - rr - x1Off); i < xx + rr + x2Off; i++) {
+       for (int j = (int) (yy - rr - y1Off); j < yy + rr + y2Off; j++) {
+           if ((i - xx) * (i - xx) + (j - yy) * (j - yy) <= rr * rr) {
+               if (wGridBounds((int) i, (int) j)) {
+            	   wGrid[(int) i][(int) j].setWID(0);
+               }
                //if(bGridBounds((int)i,(int)j)) bGrid[(int)i][(int)j]=0;
            }
        }
@@ -164,14 +184,17 @@ public class WorldGrid {
     * Deletes things in a circle of radius r
     * 
     *******************************************************************/
-   private void wPlaceCircle(int x, int y, int r, int wid, int x1Off, int x2Off, int y1Off, int y2Off){
+   private void wPlaceCircle(final int x, final int y, final int r, final int wid, final int x1Off, final int x2Off,
+		   final int y1Off, final int y2Off) {
 		double xx = x;
 		double yy = y;
 		double rr = r;
-	   for(int i = (int) (xx-rr-x1Off);i<xx+rr+x2Off;i++){
-	       for(int j = (int) (yy-rr-y1Off);j<yy+rr+y2Off;j++){
-	           if((i-xx)*(i-xx)+(j-yy)*(j-yy)<=rr*rr){
-	               if(wGridBounds((int)i,(int)j)) wGrid[(int)i][(int)j].setWID(wid);
+	   for (int i = (int) (xx - rr - x1Off); i < xx + rr + x2Off; i++) {
+	       for (int j = (int) (yy - rr - y1Off); j < yy + rr + y2Off; j++) {
+	           if ((i - xx) * (i - xx) + (j - yy) * (j - yy) <= rr * rr) {
+	               if (wGridBounds((int) i, (int) j)) {
+	            	   wGrid[(int) i][(int) j].setWID(wid);
+	               }
 	               //if(bGridBounds((int)i,(int)j)) bGrid[(int)i][(int)j]=0;
 	           }
 	       }
@@ -185,14 +208,17 @@ public class WorldGrid {
     * Deletes things in a circle of radius r
     * 
     *******************************************************************/
-   private void wUpdateLighting(int x1, int x2, int y1, int y2){
+   @SuppressWarnings("unused")
+   private void wUpdateLighting(final int x1, final int x2, final int y1, final int y2) {
        //Update Lighitng
-       for(int i = x1;i<x2;i++){
-      	 for(int j = y1;j<y2;j++){
+       for (int i = x1; i < x2; i++) {
+      	 for (int j = y1; j < y2; j++) {
       		 boolean sun = true;
-      		 for(int jj = j-1;jj>0;jj--){
-      			 if(wGrid[i][jj].getWID()!=0) sun = false;
-      			 if(!sun){
+      		 for (int jj = j - 1; jj > 0; jj--) {
+      			 if (wGrid[i][jj].getWID() != 0) {
+      				 sun = false;
+      			 }
+      			 if (!sun) {
       				 wGrid[i][j].setLight(1);
       				 //System.out.println("EY");
           			 break;
@@ -210,129 +236,152 @@ public class WorldGrid {
     * Updates water in a given area
     * 
     *******************************************************************/
-   private void wUpdateWater(int i, int j){
+   private void wUpdateWater(final int i, final int j) {
  		//Update Water
-		 if(wGrid[i][j].isWater()){
-			 if(((Block_Water) wGrid[i][j]).getWaterLevel()>1){
+		 if (wGrid[i][j].isWater()) {
+			 if (((Block_Water) wGrid[i][j]).getWaterLevel() > 1) {
 				 //Left
-	      		 if(wGridBounds(i,j+1) && wGrid[i][j+1].getWID()!=0){
-	      			 if(wGridBounds(i-1,j) && wGrid[i-1][j].getWID()==0){
-	      				setWID(i-1,j,4);
-	      				((Block_Water) wGrid[i-1][j]).setWaterLevel(1);
+	      		 if (wGridBounds(i, j + 1) && wGrid[i][j + 1].getWID() != Constants.BLOCK_AIR) {
+	      			 if (wGridBounds(i - 1, j) && wGrid[i - 1][j].getWID() == Constants.BLOCK_AIR) {
+	      				setWID(i - 1, j, 4);
+	      				((Block_Water) wGrid[i - 1][j]).setWaterLevel(1);
 	      				((Block_Water) wGrid[i][j]).decWaterLevel();
 	      			 }
-	      			 if(wGridBounds(i-1,j) && wGrid[i-1][j].isWater()){
-	      				if(((Block_Water) wGrid[i-1][j]).getWaterLevel()<((Block_Water) wGrid[i][j]).getWaterLevel()
-	      						&& ((Block_Water) wGrid[i-1][j]).getWaterLevel()<((Block_Water) wGrid[i-1][j]).getWaterLevelMax()){
-		      				((Block_Water) wGrid[i-1][j]).incWaterLevel();
+	      			 if (wGridBounds(i - 1, j) && wGrid[i - 1][j].isWater()) {
+	      				if (((Block_Water) wGrid[i - 1][j]).getWaterLevel() < ((Block_Water) wGrid[i][j]).getWaterLevel()
+	      						&& ((Block_Water) wGrid[i - 1][j]).getWaterLevel()
+	      						< ((Block_Water) wGrid[i - 1][j]).getWaterLevelMax()) {
+		      				((Block_Water) wGrid[i - 1][j]).incWaterLevel();
 		      				((Block_Water) wGrid[i][j]).decWaterLevel();
 	      				}
 	      			 }
 	      		 }
 	      		 //Right
-	      		 if(wGridBounds(i,j+1) && wGrid[i][j+1].getWID()!=0){
-	      			 if(wGridBounds(i+1,j) && wGrid[i+1][j].getWID()==0){
-	      				setWID(i+1,j,4);
+	      		 if (wGridBounds(i, j + 1) && wGrid[i][j + 1].getWID() != Constants.BLOCK_AIR) {
+	      			 if (wGridBounds(i + 1, j) && wGrid[i +1 ][j].getWID() == Constants.BLOCK_AIR) {
+	      				setWID(i + 1, j, Constants.BLOCK_WATER);
 	      				((Block_Water) wGrid[i+1][j]).setWaterLevel(1);
 	      				((Block_Water) wGrid[i][j]).decWaterLevel();
 	      			 }
-	      			 if(wGridBounds(i+1,j) && wGrid[i+1][j].isWater()){
-		      				if(((Block_Water) wGrid[i+1][j]).getWaterLevel()<((Block_Water) wGrid[i][j]).getWaterLevel()
-		      						&& ((Block_Water) wGrid[i+1][j]).getWaterLevel()<((Block_Water) wGrid[i+1][j]).getWaterLevelMax()){
-		      				((Block_Water) wGrid[i+1][j]).incWaterLevel();
+	      			 if (wGridBounds(i + 1, j) && wGrid[i + 1][j].isWater()) {
+		      				if (((Block_Water) wGrid[i + 1][j]).getWaterLevel() 
+		      						< ((Block_Water) wGrid[i][j]).getWaterLevel()
+		      						&& ((Block_Water) wGrid[i + 1][j]).getWaterLevel()
+		      						< ((Block_Water) wGrid[i + 1][j]).getWaterLevelMax()) {
+		      				((Block_Water) wGrid[i + 1][j]).incWaterLevel();
 		      				((Block_Water) wGrid[i][j]).decWaterLevel();
 	      				 }
 	      			 }
 	      		 }
 			 }
     		 //Down
-			 if(wGridBounds(i,j+1) && wGrid[i][j+1].getWID()==0){
-				setWID(i,j+1,4);
-				((Block_Water) wGrid[i][j+1]).setWaterLevel(1);
+			 if (wGridBounds(i, j + 1) && wGrid[i][j + 1].getWID() == Constants.BLOCK_AIR) {
+				setWID(i, j + 1, Constants.BLOCK_WATER);
+				((Block_Water) wGrid[i][j + 1]).setWaterLevel(1);
 				((Block_Water) wGrid[i][j]).decWaterLevel();
 			 }
-			 if(wGridBounds(i,j+1) && wGrid[i][j+1].isWater()){
-   				if(((Block_Water) wGrid[i][j+1]).getWaterLevel()<=((Block_Water) wGrid[i][j]).getWaterLevel()
-  						&& ((Block_Water) wGrid[i][j+1]).getWaterLevel()<((Block_Water) wGrid[i][j+1]).getWaterLevelMax()){
-    				((Block_Water) wGrid[i][j+1]).incWaterLevel();
+			 if (wGridBounds(i, j + 1) && wGrid[i][j + 1].isWater()) {
+   				if (((Block_Water) wGrid[i][j + 1]).getWaterLevel()
+   						<= ((Block_Water) wGrid[i][j]).getWaterLevel()
+  						&& ((Block_Water) wGrid[i][j + 1]).getWaterLevel()
+  						< ((Block_Water) wGrid[i][j + 1]).getWaterLevelMax()) {
+    				((Block_Water) wGrid[i][j + 1]).incWaterLevel();
     				((Block_Water) wGrid[i][j]).decWaterLevel();
 				}
 			 }
 			 if(((Block_Water) wGrid[i][j]).getWaterLevel()<1){
-				 setWID(i,j,0);
+				 setWID(i, j, Constants.BLOCK_AIR);
 			 }
 		 }
    }
 	
-	public void setWID(int x, int y, int wid){
+	public void setWID(final int x, final int y, final int wid){
 		//wGrid[x][y].setWID(wid);
-		if(!wGridBounds(x,y)) return;
-		Block temp = wGrid[x][y];
-		if(wid!=4){
-			wGrid[x][y] = new Block(wid,temp);
+		if (!wGridBounds(x, y)) {
+			return;
 		}
-		if(wid==4){
+		Block temp = wGrid[x][y];
+		if (wid != Constants.BLOCK_WATER) {
+			wGrid[x][y] = new Block(wid, temp);
+		}
+		if (wid == Constants.BLOCK_WATER) {
 			wGrid[x][y] = new Block_Water(temp);
 		}
 	}
 	
-	public void setBID(int x, int y, int bid){
-		if(!wGridBounds(x,y)) return;
+	public void setBID(final int x, final int y, final int bid) {
+		if (!wGridBounds(x, y)) {
+			return;
+		}
 		wGrid[x][y].setBID(bid);
 	}
 	
-	public void setLight(int x, int y, int ll){
-		if(!wGridBounds(x,y)) return;
+	public void setLight(final int x, final int y, final int ll) {
+		if (!wGridBounds(x, y)) {
+			return;
+		}
 		wGrid[x][y].setLight(ll);
 	}
 	
-	public int getWID(int x, int y){
-		if(!wGridBounds(x,y)) return 0;
+	public int getWID(final int x, final int y) {
+		if (!wGridBounds(x, y)) {
+			return 0;
+		}
 		return wGrid[x][y].getWID();
 	}
 	
-	public int getBID(int x, int y){
-		if(!wGridBounds(x,y)) return 0;
+	public int getBID(final int x, final int y) {
+		if (!wGridBounds(x, y)) {
+			return 0;
+		}
 		return wGrid[x][y].getBID();
 	}
 	
-	public int getLight(int x, int y){
-		if(!wGridBounds(x,y)) return 0;
+	public int getLight(final int x, final int y) {
+		if (!wGridBounds(x, y)) {
+			return 0;
+		}
 		return wGrid[x][y].getLight();
 	}
 	
-	public boolean isWater(int x, int y){
-		if(!wGridBounds(x,y)) return false;
+	public boolean isWater (final int x, final int y) {
+		if (!wGridBounds(x, y)) {
+			return false;
+		}
 		return wGrid[x][y].isWater();
 	}
 	
-	public int getWaterLevel(int x, int y){
-		if(!wGridBounds(x,y)) return 0;
+	public int getWaterLevel(final int x, final int y) {
+		if (!wGridBounds(x, y)) {
+			return 0;
+		}
 		return ((Block_Water) wGrid[x][y]).getWaterLevel();
 	}
 	
-	public int blockSize(){
+	public int blockSize() {
 		return blockSize;
 	}
 	
-	public int sizeX(){
+	public int sizeX() {
 		return sizeX;
 	}
 	
-	public int sizeY(){
+	public int sizeY() {
 		return sizeY;
 	}
 	
-	public void update(int x1, int x2, int y1, int y2){
-       for(int i = x1;i<x2;i++){
-      	 for(int j = y1;j<y2;j++){
-      		if(!wGridBounds(i,j)) continue;
-      		 wUpdateWater(i,j);
+	public void update(final int x1, final int x2, final int y1, final int y2) {
+       for (int i = x1; i < x2; i++) {
+      	 for (int j = y1; j < y2; j++) {
+      		if (!wGridBounds(i, j)) {
+      			continue;
+      		}
+      		 wUpdateWater(i, j);
       	 }
        }
 	}
 	
-   private boolean wGridBounds(int i, int j){
-	   return i>=0 && i<sizeX && j>=0 && j<sizeY;
+   private boolean wGridBounds(final int i, final int j) {
+	   return i >= 0 && i < sizeX && j >= 0 && j < sizeY;
    }
 }
