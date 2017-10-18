cfe08d1fe18ce6103e3f2dfb5e99358df9e91821
deleonl <deleonl@mail.gvsu.edu>
2017-10-18 05:50:06 -0400

    More checkstyle and javadoc

--- src/main/Item.java
+++ src/main/Item.java
@@ -1,29 +1,89 @@
 package main;
 
 public class Item {
+	/**
+	 * The type of item.
+	 */
 	private int type;
+	
+	/**
+	 * The id of the item.
+	 */
 	private int id;
+	
+	/**
+	 * The number of this item.
+	 */
 	private int count;
 	
-	public Item(int t, int i, int c){
+	/**
+	 * The constructor creates a new Item of type t,
+	 * with id i, and count c. 
+	 * @param t the type of item.
+	 * @param i the id of the item.
+	 * @param c the number of the item.
+	 */
+	public Item(final int t, final int i, final int c) {
 		type = t;
 		id = i;
 		count = c;
 		//type = ItemType.BLOCK;
 	}
 	
-	public int getType(){return type;}
+	/**
+	 * Gets the type of item.
+	 * @return the item type.
+	 */
+	public int getType() {
+		return type;
+	}
 	
-	public int getId(){return id;}
+	/**
+	 * Gets the item's id.
+	 * @return the item's id.
+	 */
+	public int getId() {
+		return id;
+	}
 	
-	public int getCount(){return count;}
+	/**
+	 * Gets the count of the item.
+	 * @return the item count.
+	 */
+	public int getCount() {
+		return count;
+	}
 	
-	public void setType(int t){type = t;}
+	/**
+	 * Sets the type of the item.
+	 * @param t the value to set type to.
+	 */
+	public void setType(final int t) {
+		type = t;
+	}
 	
-	public void setId(int i){id = i;}
+	/**
+	 * Sets the id of the item.
+	 * @param i the value to set id to.
+	 */
+	public void setId(final int i) {
+		id = i;
+	}
 	
-	public void setCount(int c){count = c;}
+	/**
+	 * Sets the count of the item.
+	 * @param c the value to set count to.
+	 */
+	public void setCount(final int c) {
+		count = c;
+	}
 	
-	public void changeCount(int c){count+=c;}
+	/**
+	 * Increments the count by a certain amount.
+	 * @param c the amount to increment count by.
+	 */
+	public void changeCount(final int c) {
+		count += c;
+	}
 	
 }
--- src/main/Item_Drop.java
+++ src/main/Item_Drop.java
@@ -1,10 +1,38 @@
 package main;
 
-public class Item_Drop extends WorldObject{
-	int type;
-	int id;
-	int count;
-	public Item_Drop(double xx, double yy, int t, int i, int c) {
+/**
+ * The icons the player picks up after they destroy a block,
+ * kill an entity, etc.
+ * @author Gary Fleming
+ *
+ */
+public class Item_Drop extends WorldObject {
+	/**
+	 * The type of item.
+	 */
+	private int type;
+	
+	/**
+	 * The id of the item.
+	 */
+	private int id;
+	
+	/**
+	 * The amount of the item.
+	 */
+	private int count;
+	
+	/**
+	 * The constructor sets the x and y location of the drop. It
+	 * also sets the type, id, and count of the item.
+	 * @param xx the x location of the item.
+	 * @param yy the y location of the item.
+	 * @param t the type of the item.
+	 * @param i the id of the item.
+	 * @param c the amount of the item.
+	 */
+	public Item_Drop(final double xx, final double yy, final int t,
+			final int i, final int c) {
 		super(xx, yy);
 		width = 32;
 		height = 32;
@@ -13,23 +41,75 @@ public class Item_Drop extends WorldObject{
 		count = c;
 	}
 	
-	public int getType(){return type;}
+	/**
+	 * Gets the type of the item drop.
+	 * @return the type of the item drop.
+	 */
+	public int getType() {
+		return type;
+	}
 	
-	public int getId(){return id;}
+	/**
+	 * Gets the id of the item drop.
+	 * @return the id of the item drop.
+	 */
+	public int getId() {
+		return id;
+	}
 	
-	public int getCount(){return count;}
+	/**
+	 * Gets the amount of the item.
+	 * @return the number of that item.
+	 */
+	public int getCount() {
+		return count;
+	}
 	
-	public void setType(int t){type = t;}
+	/**
+	 * Sets the type of the item drop.
+	 * @param t the value to set type to.
+	 */
+	public void setType(final int t) {
+		type = t;
+	}
 	
-	public void setId(int i){id = i;}
+	/**
+	 * Sets the id of the item drop.
+	 * @param i the value to set id to.
+	 */
+	public void setId(final int i) {
+		id = i;
+	}
 	
-	public void setCount(int c){count = c;}
+	/**
+	 * Sets the amount of an item drop.
+	 * @param c the value to set count to.
+	 */
+	public void setCount(final int c) {
+		count = c;
+	}
 	
-	public void changeCount(int c){count+=c;}
+	/**
+	 * Increment the count by a value.
+	 * @param c the value to increment count by.
+	 */
+	public void changeCount(final int c) {
+		count += c;
+	}
 	
-	public void step(Game g){
-		if(xsp<0.1) xsp=0;
-		if(ysp<0.1) ysp=0;
+	/**
+	 * The actions taken by the ItemDrop during
+	 * each iteration of the game. It stops if
+	 * it starts to go too slowly.
+	 * @param g the game the item drop is in.
+	 */
+	public void step(final Game g) {
+		if (xsp < 0.1) {
+			xsp = 0;
+		}
+		if (ysp < 0.1) {
+			ysp = 0;
+		}
 		super.step(g);
 	}
 
