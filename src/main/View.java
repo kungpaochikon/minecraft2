a81d4f223cf397ca69b0a0318e075bc686b76e7d
deleonl <deleonl@mail.gvsu.edu>
2017-10-18 07:12:07 -0400

    View Javadoc and checkstyle

--- src/main/View.java
+++ src/main/View.java
@@ -1,22 +1,82 @@
 package main;
 
 import java.util.Random;
-
+/**
+ * The game view. This controls the view shaking when
+ * the player character falls from high up.
+ * @author Gary Fleming, Logun DeLeon, Alex Duncanson
+ *
+ */
 public class View {
+	/**
+	 * The normal x coordinate of the view.
+	 */
 	private double viewX;
+	
+	/**
+	 * The normal y coordinate of the view.
+	 */
 	private double viewY;
+	
+	/**
+	 * The actual x coordinate of the view.
+	 */
 	private double viewXFinal;
+	
+	/**
+	 * The actual y coordinate of the view.
+	 */
 	private double viewYFinal;
+	
+	/**
+	 * The width of the view.
+	 */
 	private int viewW;
+	
+	/**
+	 * The height of the view.
+	 */
 	private int viewH;
+	
+	/**
+	 * The x offset of the view caused by
+	 * the screen shaking.
+	 */
 	private int viewShakeX;
+	
+	/**
+	 * The y offset of the view caused by
+	 * the screen shaking.
+	 */
 	private int viewShakeY;
+	
+	/**
+	 * The amount of time that the
+	 * screen shakes.
+	 */
 	private int viewShakeTime;
+	
+	/**
+	 * The magnitude at which the screen
+	 * shakes.
+	 */
    	private int viewShakeMag;
+   	
+   	/**
+   	 * If the view is shaking.
+   	 */
    	private boolean viewShaking;
+   	
+   	/**
+   	 * A random number generator.
+   	 */
    	private Random random;
    	
-   	public View(){
+   	/**
+   	 * A constructor that sets everything to default values.
+   	 * All numerical values are set to 0. viewShaking is false.
+   	 */
+   	public View() {
    		setViewX(0);
    		setViewY(0);
    		setViewXFinal(0);
@@ -31,7 +91,14 @@ public class View {
    		random = new Random();
    	}
    	
-   	public View(double x, double y, int w, int h){
+   	/**
+   	 * A constructor that allows you to set viewX, viewY, width, and height.
+   	 * @param x the value of viewX.
+   	 * @param y the value of viewY.
+  	 * @param w the width of the view.
+   	 * @param h the height of the view.
+   	 */
+   	public View(final double x, final double y, final int w, final int h) {
    		this();
    		setViewX(x);
    		setViewY(y);
@@ -39,17 +106,20 @@ public class View {
    		setViewH(h);
    	}
    	
-   	public void step(){
+   	/**
+   	 * The actions taken by the view during each iteration of
+   	 * the game loop. Shakes the screen if necessary.
+   	 */
+   	public void step() {
  	   //View Shake
- 	   if(viewShaking){
+ 	   if (viewShaking) {
  		   viewShakeTime--;
- 		   if(viewShakeTime<1){
+ 		   if (viewShakeTime < 1) {
  			   viewShaking = false;
  		   }
- 		   viewShakeX = random.nextInt(viewShakeMag*2 + 1) - viewShakeMag;
- 		   viewShakeY = random.nextInt(viewShakeMag*2 + 1) - viewShakeMag;
- 	   }
- 	   else{
+ 		   viewShakeX = random.nextInt(viewShakeMag * 2 + 1) - viewShakeMag;
+ 		   viewShakeY = random.nextInt(viewShakeMag * 2 + 1) - viewShakeMag;
+ 	   } else {
  		   viewShakeX = 0;
  		   viewShakeY = 0;
  	   }
@@ -66,100 +136,190 @@ public class View {
      * -----------
      * Init the camera shake.
      * Will change to a list later to allow for multiple shakes
+     * @param time the amount of time the screen shakes.
+     * @param mag the magnitude that the screen shakes.
      * 
      *******************************************************************/
-    public void viewShake(int time, int mag){
+    public void viewShake(final int time, final int mag) {
  	   viewShakeTime = time;
  	   viewShakeMag = mag;
  	   viewShaking = true;
     }
     
-
+    /**
+     * Gets the viewX.
+     * @return the viewX.
+     */
 	public double getViewX() {
 		return viewX;
 	}
-
-	public void setViewX(double viewX) {
+	
+	/**
+	 * Sets the viewX.
+	 * @param viewX the value to set viewX to.
+	 */
+	public void setViewX(final double viewX) {
 		this.viewX = viewX;
 	}
-
+	
+	/**
+	 * Gets the viewY.
+	 * @return the viewY
+	 */
 	public double getViewY() {
 		return viewY;
 	}
-
-	public void setViewY(double viewY) {
+	
+	/**
+	 * Sets the viewY.
+	 * @param viewY the value to set viewY to.
+	 */
+	public void setViewY(final double viewY) {
 		this.viewY = viewY;
 	}
-
+	
+	/**
+	 * Gets the actual viewX.
+	 * @return the actual viewX.
+	 */
 	public double getViewXFinal() {
 		return viewXFinal;
 	}
-
-	public void setViewXFinal(double viewXFinal) {
+	
+	/**
+	 * Set the actual viewX.
+	 * @param viewXFinal the value to set this.viewXFinal to.
+	 */
+	public void setViewXFinal(final double viewXFinal) {
 		this.viewXFinal = viewXFinal;
 	}
-
+	
+	/**
+	 * Get the actual viewY.
+	 * @return the actual viewY.
+	 */
 	public double getViewYFinal() {
 		return viewYFinal;
 	}
-
-	public void setViewYFinal(double viewYFinal) {
+	
+	/**
+	 * Set the actual viewY.
+	 * @param viewYFinal the value to set this.viewYFinal to.
+	 */
+	public void setViewYFinal(final double viewYFinal) {
 		this.viewYFinal = viewYFinal;
 	}
-
+	
+	/**
+	 * Get the width of the view.
+	 * @return the width of the view.
+	 */
 	public int getViewW() {
 		return viewW;
 	}
-
-	public void setViewW(int viewW) {
+	
+	/**
+	 * Set the width of the view.
+	 * @param viewW the value to set the width
+	 * of the view to.
+	 */
+	public void setViewW(final int viewW) {
 		this.viewW = viewW;
 	}
-
+	
+	/**
+	 * Get the height of the view.
+	 * @return the height of the view.
+	 */
 	public int getViewH() {
 		return viewH;
 	}
-
-	public void setViewH(int viewH) {
+	
+	/**
+	 * Set the height of the view.
+	 * @param viewH the value to set the height to.
+	 */
+	public void setViewH(final int viewH) {
 		this.viewH = viewH;
 	}
-
+	
+	/**
+	 * Get the x offset due to shaking.
+	 * @return the x offset due to shaking.
+	 */
 	public int getViewShakeX() {
 		return viewShakeX;
 	}
-
-	public void setViewShakeX(int viewShakeX) {
+	
+	/**
+	 * Set the x offset due to shaking.
+	 * @param viewShakeX the value to set this.viewShakeX to.
+	 */
+	public void setViewShakeX(final int viewShakeX) {
 		this.viewShakeX = viewShakeX;
 	}
-
+	
+	/**
+	 * Get the y offset due to shaking.
+	 * @return the y offset due to shaking.
+	 */
 	public int getViewShakeY() {
 		return viewShakeY;
 	}
-
-	public void setViewShakeY(int viewShakeY) {
+	
+	/**
+	 * Set the y offset due to shaking.
+	 * @param viewShakeY the value to set this.viewShakeY to.
+	 */
+	public void setViewShakeY(final int viewShakeY) {
 		this.viewShakeY = viewShakeY;
 	}
-
+	
+	/**
+	 * Gets the time the view shakes for.
+	 * @return the time the view shakes for.
+	 */
 	public int getViewShakeTime() {
 		return viewShakeTime;
 	}
-
-	public void setViewShakeTime(int viewShakeTime) {
+	
+	/**
+	 * Set the time the view shakes for.
+	 * @param viewShakeTime the value to set this.viewShakeTime to.
+	 */
+	public void setViewShakeTime(final int viewShakeTime) {
 		this.viewShakeTime = viewShakeTime;
 	}
-
+	
+	/**
+	 * Gets the magnitude that the view shakes at.
+	 * @return the magnitude of the view shake.
+	 */
 	public int getViewShakeMag() {
 		return viewShakeMag;
 	}
-
-	public void setViewShakeMag(int viewShakeMag) {
+	
+	/**
+	 * Set the magnitude of the view shake.
+	 * @param viewShakeMag the value to set this.viewShakeMag to.
+	 */
+	public void setViewShakeMag(final int viewShakeMag) {
 		this.viewShakeMag = viewShakeMag;
 	}
-
+	
+	/**
+	 * Returns if the view is shaking or not.
+	 * @return if the view is shaking or not.
+	 */
 	public boolean isViewShaking() {
 		return viewShaking;
 	}
-
-	public void setViewShaking(boolean viewShaking) {
+	
+	/**
+	 * Sets whether the view is shaking or not.
+	 * @param viewShaking the value to set viewShaking to.
+	 */
+	public void setViewShaking(final boolean viewShaking) {
 		this.viewShaking = viewShaking;
 	}
 	
