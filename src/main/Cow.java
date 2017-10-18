4b72609b92f5c3f06969268926f2b155dd4b8eb4
deleonl <deleonl@mail.gvsu.edu>
2017-10-18 06:31:43 -0400

    Cow Checkstyle and javadoc

--- src/main/Cow.java
+++ src/main/Cow.java
@@ -2,10 +2,28 @@ package main;
 
 import java.util.Random;
 
-public class Cow extends Animal{
-	int state;
-	Random random;
-	public Cow(double xx, double yy) {
+/**
+ * A cow entity.
+ * @author Gary Fleming, Logun DeLeon, Alexander Duncanson
+ *
+ */
+public class Cow extends Animal {
+	/**
+	 * The current state of the cow.
+	 */
+	private int state;
+	
+	/**
+	 * A random number generator.
+	 */
+	private Random random;
+	
+	/**
+	 * This constructor creates a cow with a set x and y.
+	 * @param xx the x coordinate of the cow.
+	 * @param yy the y coordinate of the cow.
+	 */
+	public Cow(final double xx, final double yy) {
 		super(xx, yy);
 		width = 64;
 		height = 64;
@@ -13,7 +31,12 @@ public class Cow extends Animal{
 		random = new Random();
 	}
 	
-	public void step(Game g){
+	/**
+	 * The actions a cow takes during each iteration
+	 * of the game loop.
+	 * @param g the game the cow exists in.
+	 */
+	public void step(final Game g) {
 		super.step(g);
 	}
 
