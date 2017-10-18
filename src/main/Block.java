44506436429a4d26415a5f0e1cabd01a775e37ce
Alex Duncanson <duncansa@mail.gvsu.edu>
2017-10-18 07:25:23 -0400

    checkstyle changes

--- src/main/Block.java
+++ src/main/Block.java
@@ -1,12 +1,31 @@
 package main;
 
+/**
+ * @author gary, logun, alex
+ *
+ */
 public class Block {
 	
+	/**
+	 * 
+	 */
 	private int wID;
+	/**
+	 * 
+	 */
 	private int bID;
+	/**
+	 * 
+	 */
 	private int light;
+	/**
+	 * 
+	 */
 	private int integrity;
 	
+	/**
+	 * 
+	 */
 	public Block() {
 		wID = 0;
 		bID = 0;
@@ -14,6 +33,11 @@ public class Block {
 		integrity = Constants.INTEGRITIES[wID];
 	}
 	
+	/**
+	 * @param wid world ID
+	 * @param bid block ID
+	 * @param ll light
+	 */
 	public Block(final int wid, final int bid, final int ll) {
 		wID = wid;
 		bID = bid;
@@ -21,6 +45,10 @@ public class Block {
 		integrity = Constants.INTEGRITIES[wID];
 	}
 	
+	/**
+	 * @param wid width id
+	 * @param block block id
+	 */
 	public Block(final int wid, final Block_Water block) {
 		wID = wid;
 		bID = block.getBID();
@@ -29,7 +57,11 @@ public class Block {
 		
 	}
 	
-	public Block(final int wid, final Block block){
+	/**
+	 * @param wid width id
+	 * @param block block id
+	 */
+	public Block(final int wid, final Block block) {
 		wID = wid;
 		bID = block.getBID();
 		light = block.getLight();
@@ -37,35 +69,59 @@ public class Block {
 		
 	}
 	
+	/**
+	 * @param wid width id
+	 */
 	public void setWID(final int wid) {
 		wID = wid;
 		integrity = Constants.INTEGRITIES[wID];
 	}
 	
+	/**
+	 * @param bid block id
+	 */
 	public void setBID(final int bid) {
 		bID = bid;
 	}
 	
+	/**
+	 * @param ll light
+	 */
 	public void setLight(final int ll) {
 		light = ll;
 	}
 	
+	/**
+	 * @return world id
+	 */
 	public int getWID() {
 		return wID;
 	}
 	
+	/**
+	 * @return block id
+	 */
 	public int getBID() {
 		return bID;
 	}
 	
+	/**
+	 * @return light
+	 */
 	public int getLight() {
 		return light;
 	}
 	
+	/**
+	 * @return if the block is water or not
+	 */
 	public boolean isWater() {
 		return this instanceof Block_Water;
 	}
 	
+	/**
+	 * @return the integrity of the block
+	 */
 	public int getIntegrity() {
 		return integrity;
 	}
@@ -77,6 +133,9 @@ public class Block {
 		integrity -= damage;
 	}
 	
+	/**
+	 * 
+	 */
 	public void resetIntegrity() {
 		integrity = Constants.INTEGRITIES[wID];
 	}
